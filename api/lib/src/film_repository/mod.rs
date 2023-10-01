97eeae070a3f97246734f598487a4087233b9ff5
Heinea K. Chen <24603835+sparkyvxcx@users.noreply.github.com>
2023-10-01 22:53:44 +1100

    Add postgres film repository module

--- api/lib/src/film_repository/mod.rs
+++ api/lib/src/film_repository/mod.rs
@@ -1,5 +1,9 @@
-use shared::models::Film;
+use shared::models::{CreateFilm, Film};
 use uuid::Uuid;
+
+pub use postgres_film_repository::PostgresFilmRepository;
+
+mod postgres_film_repository;
 
 pub type FilmError = String;
 pub type FilmResult<T> = Result<T, FilmError>;
@@ -8,7 +12,7 @@ pub type FilmResult<T> = Result<T, FilmError>;
 pub trait FilmRepository: Send + Sync + 'static {
     async fn get_films(&self) -> FilmResult<Vec<Film>>;
     async fn get_film(&self, id: &Uuid) -> FilmResult<Film>;
-    async fn create_film() -> FilmResult<Film>;
-    async fn update_film() -> FilmResult<Film>;
-    async fn delete_film() -> FilmResult<Uuid>;
+    async fn create_film(&self, id: &CreateFilm) -> FilmResult<Film>;
+    async fn update_film(&self, id: &Film) -> FilmResult<Film>;
+    async fn delete_film(&self, id: &Uuid) -> FilmResult<Uuid>;
 }
--- shared/src/models.rs
+++ shared/src/models.rs
@@ -2,17 +2,20 @@ use chrono;
 use serde::{Deserialize, Serialize};
 use uuid;
 
+#[cfg_attr(feature = "backend", derive(sqlx::FromRow))]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Film {
     pub id: uuid::Uuid,
     pub title: String,
     pub director: String,
+    #[sqlx(try_from = "i16")]
     pub year: u16,
     pub poster: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
+#[cfg_attr(feature = "backend", derive(sqlx::FromRow))]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct CreateFilm {
     pub title: String,
