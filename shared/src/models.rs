b6a24101a4c12cb00e82d68e59cc4561f79be266
Heinea K. Chen <24603835+sparkyvxcx@users.noreply.github.com>
2023-10-01 21:21:27 +1100

    Add models

---
+++ shared/src/models.rs
@@ -0,0 +1,18 @@
+use chrono;
+use uuid;
+pub struct Film {
+    pub id: uuid::Uuid,
+    pub title: String,
+    pub director: String,
+    pub year: u16,
+    pub poster: String,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+pub struct CreateFilm {
+    pub title: String,
+    pub director: String,
+    pub year: u16,
+    pub poster: String,
+}
