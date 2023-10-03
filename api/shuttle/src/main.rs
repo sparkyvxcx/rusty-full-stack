e08e9a4207e77338d9d9c8d5cf8062adab8e4b53
Heinea K. Chen <24603835+sparkyvxcx@users.noreply.github.com>
2023-10-04 00:04:42 +1100

    Add capability to serve static files

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -2,6 +2,7 @@ use actix_web::web::{self, ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
+use std::path::PathBuf;
 
 use api_lib::film_repository::PostgresFilmRepository;
 use api_lib::routes::{hello_world, ping, version};
@@ -10,6 +11,7 @@ use api_lib::{films, health};
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
+    #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: PathBuf,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     // initialize the database if not already initialized
     pool.execute(include_str!("../../db/schema.sql"))
@@ -19,12 +21,20 @@ async fn actix_web(
     let film_repo = api_lib::film_repository::PostgresFilmRepository::new(pool);
     let film_repo = web::Data::new(film_repo);
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(film_repo)
-            .configure(health::service)
-            .configure(films::service::<PostgresFilmRepository>)
-            .service(hello_world)
-            .service(ping)
-            .service(version);
+        cfg.service(
+            web::scope("/api")
+                .app_data(film_repo)
+                .configure(health::service)
+                .configure(films::service::<PostgresFilmRepository>),
+        )
+        .service(hello_world)
+        .service(ping)
+        .service(version)
+        .service(
+            actix_files::Files::new("/", static_folder)
+                .show_files_listing()
+                .index_file("index.html"),
+        );
     };
 
     Ok(config.into())
